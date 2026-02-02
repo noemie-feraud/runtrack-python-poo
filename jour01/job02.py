d5dba4750b5c241dac246b9f513101ae449ce129
noemie-feraud <noemie.feraud@laplateforme.io>
2026-02-02 16:16:28 +0100

    Insert print with numbers value

---
+++ jour01/job02.py
@@ -0,0 +1,6 @@
+class operation:
+    def __init__(self, nombre1, nombre2 ):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+mon_operation = operation(35, 23)
+print(f"Le nombre1 est {mon_operation.nombre1}\nLe nombre2 est {mon_operation.nombre2}")
